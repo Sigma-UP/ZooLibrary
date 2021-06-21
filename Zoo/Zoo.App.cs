2cf2375987bab52c312ca45d50f973cd6146761c
Sigma-UP <62567341+Sigma-UP@users.noreply.github.com>
2021-06-21 23:21:07 +0300

    add Zoo sln project

---
+++ Zoo/Zoo.App.cs
@@ -0,0 +1,23 @@
+﻿using ZooLib;
+using ZooLib.Animals;
+using ZooLib.Animals.Birds;
+using ZooLib.Animals.Mammals;
+using ZooLib.Animals.Reptiles;
+using ZooLib.Console;
+using ZooLib.Employees;
+using ZooLib.Exceptions;
+using ZooLib.Foods;
+using ZooLib.Medicines;
+using System.Collections.Generic;
+using System;
+
+namespace Zoo
+{
+    public static class Application
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
